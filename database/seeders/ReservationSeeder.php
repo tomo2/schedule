6d9897f2be8b1fcd72d8f077b97101db50c5c9ac
tomo <rhbgp7kt@gmail.com>
2024-02-28 01:09:53 +0900

    reservation

---
+++ database/seeders/ReservationSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class ReservationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('reservations')->insert([
+            [
+                'user_id' => 1,
+                'event_id' => 1,
+            ],
+            [
+                'user_id' => 2,
+                'event_id' => 1,
+            ],
+            [
+                'user_id' => 1,
+                'event_id' => 2,
+            ]
+            ]);
+    }
+}
