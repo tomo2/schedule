99dfad43db875fe6afb20ed0d2ba97f330e0e92e
tomo <rhbgp7kt@gmail.com>
2024-03-29 03:17:14 +0900

    livewire接続

--- resources/views/livewire/calculation.blade.php
+++ resources/views/livewire/calculation.blade.php
@@ -1,11 +1,74 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>TEST</title>
-    @livewireStyles
 </head>
 <body>
-<h1>Livewireてすと</h1>
-@livewireScripts
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="max-w-2x1 mx-auto">
+            <x-validation-errors class="mb-4" />
+
+            @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    {{ session('status') }}
+                </div>
+            @endif
+    
+            <form method="POST" action="{{ route('events.store') }}">
+                @csrf
+                {{-- nameでflatpickrを利用 --}}
+                <div>
+                    <x-label for="name" value="名前" />
+                    <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+
+                </div>
+
+                    <div class="mt-4">
+                        <x-label for="care" value="care" />
+                        <select name="care" class="block mt-1 w-full" type="text" name="start_time" required >
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+
+                    {{-- <div class="md:flex justify-between"> --}}
+                    <div class="mt-4">
+                        <x-label for="date" value="利用日" />
+                        <x-input id="date" class="block mt-1 w-full" type="text" name="date" required />
+                    </div>
+                    
+                    <div class="mt-4">
+                        <x-label for="start_time" value="開始時間" />
+                        <x-input id="start_time" class="block mt-1 w-full" type="text" name="start_time" required />
+                    </div>
+                    <div class="mt-4">
+                        <x-label for="end_time" value="終了時間" />
+                        <x-input id="end_time" class="block mt-1 w-full" type="text" name="end_time" required />
+                    </div>
+
+                    <div class="mt-4">
+                        <x-label for="price" value="料金" />
+                        <x-input" id="price" class="block mt-1 w-full" type="text" name="price" required />
+                    </div>
+
+@foreach($startTime as $startTimes)
+    123
+@endforeach
+
+                    <x-button class="ml-4 mt-4">
+                        新規登録
+                    </x-button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 </body>
 </html>
--- resources/views/manager/events/create.blade.php
+++ resources/views/manager/events/create.blade.php
@@ -5,68 +5,8 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="max-w-2x1 mx-auto">
-                <x-validation-errors class="mb-4" />
 
-                @if (session('status'))
-                    <div class="mb-4 font-medium text-sm text-green-600">
-                        {{ session('status') }}
-                    </div>
-                @endif
-        
-                <form method="POST" action="{{ route('events.store') }}">
-                    @csrf
-                    {{-- nameでflatpickrを利用 --}}
-                    <div>
-                        <x-label for="name" value="名前" />
-                        <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            @livewire('calculation')
 
-                    </div>
 
-                        <div class="mt-4">
-                            <x-label for="care" value="care" />
-                            <select name="care" class="block mt-1 w-full" type="text" name="start_time" required >
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>
-                        </div>
-
-                        {{-- <div class="md:flex justify-between"> --}}
-                        <div class="mt-4">
-                            <x-label for="date" value="利用日" />
-                            <x-input id="date" class="block mt-1 w-full" type="text" name="date" required />
-                        </div>
-                        
-                        <div class="mt-4">
-                            <x-label for="start_time" value="開始時間" />
-                            <x-input id="start_time" class="block mt-1 w-full" type="text" name="start_time" required />
-                        </div>
-                        <div class="mt-4">
-                            <x-label for="end_time" value="終了時間" />
-                            <x-input id="end_time" class="block mt-1 w-full" type="text" name="end_time" required />
-                        </div>
-
-                        
-
-                        <div class="mt-4">
-                            <x-label for="price" value="料金" />
-                            <x-input" id="price" class="block mt-1 w-full" type="text" name="price" required />
-                        </div>
-
-                        {{-- </div> --}}
-
-                    <x-button class="ml-4 mt-4">
-                        新規登録
-                    </x-button>
-                </form>
-            </div>
-            </div>
-        </div>
-    </div>
 </x-app-layout>
