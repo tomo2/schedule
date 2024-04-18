169dd47c3d29a1b324fd488ad340c980feda9b9e
tomo <rhbgp7kt@gmail.com>
2024-04-19 02:43:41 +0900

    editページの修正

--- resources/views/manager/events/edit.blade.php
+++ resources/views/manager/events/edit.blade.php
@@ -56,7 +56,6 @@
 
                 {{-- buttonを指定することでformに影響を与えない --}}
 
-                後ほどボタンを押せるようにする
                 <div class="mt-6 mx-auto w-2/3">
                     <x-label for="time" value="利用時間" />
                 </div>
@@ -73,67 +72,62 @@
                             </tr>
                             <tr>
                                 <td class="px-4 text-xs">要介護1</td>
-                                <td><x-button2 type="button" value="368">要介護1 3~4時間</x-button2></td>
-                                <td><x-button2 type="button" value="386">要介護1 4~5時間</x-button2></td>
-                                <td><x-button2 type="button" value="567">要介護1 5~6時間</x-button2></td>
-                                <td><x-button2 type="button" value="581">要介護1 6~7時間</x-button2></td>
-                                <td><x-button2 type="button" value="655">要介護1 7~8時間</x-button2></td>
-                                <td><x-button2 type="button" value="666">要介護1 8~9時間</x-button2></td>
+                                <td><x-button2 type="button" value="368" onclick="number(368)">要介護1 3~4時間</x-button2></td>
+                                <td><x-button2 type="button" value="386" onclick="number(386)">要介護1 4~5時間</x-button2></td>
+                                <td><x-button2 type="button" value="567" onclick="number(567)">要介護1 5~6時間</x-button2></td>
+                                <td><x-button2 type="button" value="581" onclick="number(581)">要介護1 6~7時間</x-button2></td>
+                                <td><x-button2 type="button" value="655" onclick="number(655)">要介護1 7~8時間</x-button2></td>
+                                <td><x-button2 type="button" value="666" onclick="number(666)">要介護1 8~9時間</x-button2></td>
                             </tr>
                             <tr>
                                 <td class="px-4 text-xs">要介護2</td>
-                                <td><x-button2 type="button" value="421">要介護2 3~4時間</x-button2></td>
-                                <td><x-button2 type="button" value="442">要介護2 4~5時間</x-button2></td>
-                                <td><x-button2 type="button" value="670">要介護2 5~6時間</x-button2></td>
-                                <td><x-button2 type="button" value="686">要介護2 6~7時間</x-button2></td>
-                                <td><x-button2 type="button" value="773">要介護2 7~8時間</x-button2></td>
-                                <td><x-button2 type="button" value="787">要介護2 8~9時間</x-button2></td>
+                                <td><x-button2 type="button" value="421" onclick="number(421)">要介護2 3~4時間</x-button2></td>
+                                <td><x-button2 type="button" value="442" onclick="number(442)">要介護2 4~5時間</x-button2></td>
+                                <td><x-button2 type="button" value="670" onclick="number(670)">要介護2 5~6時間</x-button2></td>
+                                <td><x-button2 type="button" value="686" onclick="number(686)">要介護2 6~7時間</x-button2></td>
+                                <td><x-button2 type="button" value="773" onclick="number(773)">要介護2 7~8時間</x-button2></td>
+                                <td><x-button2 type="button" value="787" onclick="number(787)">要介護2 8~9時間</x-button2></td>
                             </tr>
                             <tr>
                                 <td class="px-4 text-xs">要介護3</td>
-                                <td><x-button2 type="button" value="477">要介護3 3~4時間</x-button2></td>
-                                <td><x-button2 type="button" value="500">要介護3 4~5時間</x-button2></td>
-                                <td><x-button2 type="button" value="773">要介護3 5~6時間</x-button2></td>
-                                <td><x-button2 type="button" value="792">要介護3 6~7時間</x-button2></td>
-                                <td><x-button2 type="button" value="896">要介護3 7~8時間</x-button2></td>
-                                <td><x-button2 type="button" value="911">要介護3 8~9時間</x-button2></td>
+                                <td><x-button2 type="button" value="477" onclick="number(477)">要介護3 3~4時間</x-button2></td>
+                                <td><x-button2 type="button" value="500" onclick="number(500)">要介護3 4~5時間</x-button2></td>
+                                <td><x-button2 type="button" value="773" onclick="number(773)">要介護3 5~6時間</x-button2></td>
+                                <td><x-button2 type="button" value="792" onclick="number(792)">要介護3 6~7時間</x-button2></td>
+                                <td><x-button2 type="button" value="896" onclick="number(896)">要介護3 7~8時間</x-button2></td>
+                                <td><x-button2 type="button" value="911" onclick="number(911)">要介護3 8~9時間</x-button2></td>
                             </tr>
                             <tr>
                                 <td class="px-4 text-xs">要介護4</td>
-                                <td><x-button2 type="button" value="530">要介護4 3~4時間</x-button2></td>
-                                <td><x-button2 type="button" value="557">要介護4 4~5時間</x-button2></td>
-                                <td><x-button2 type="button" value="876">要介護4 5~6時間</x-button2></td>
-                                <td><x-button2 type="button" value="897">要介護4 6~7時間</x-button2></td>
-                                <td><x-button2 type="button" value="1018">要介護4 7~8時間</x-button2></td>
-                                <td><x-button2 type="button" value="1036">要介護4 8~9時間</x-button2></td>
+                                <td><x-button2 type="button" value="530" onclick="number(530)">要介護4 3~4時間</x-button2></td>
+                                <td><x-button2 type="button" value="557" onclick="number(557)">要介護4 4~5時間</x-button2></td>
+                                <td><x-button2 type="button" value="876" onclick="number(876)">要介護4 5~6時間</x-button2></td>
+                                <td><x-button2 type="button" value="897" onclick="number(897)">要介護4 6~7時間</x-button2></td>
+                                <td><x-button2 type="button" value="1018" onclick="number(1018)">要介護4 7~8時間</x-button2></td>
+                                <td><x-button2 type="button" value="1036" onclick="number(1036)">要介護4 8~9時間</x-button2></td>
                             </tr>
                             <tr>
                                 <td class="px-4 text-xs">要介護5</td>
-                                <td><x-button2 type="button" value="585">要介護5 3~4時間</x-button2></td>
-                                <td><x-button2 type="button" value="614">要介護5 4~5時間</x-button2></td>
-                                <td><x-button2 type="button" value="979">要介護5 5~6時間</x-button2></td>
-                                <td><x-button2 type="button" value="1003">要介護5 6~7時間</x-button2></td>
-                                <td><x-button2 id="button" type="button" value="1142" onclick="number()">要介護5 7~8時間</x-button2></td>
-                                <td><x-button2 id="button" type="button" value="1162" onclick="number()">要介護5 8~9時間</x-button2></td>
+                                <td><x-button2 type="button" value="585" onclick="number(585)">要介護5 3~4時間</x-button2></td>
+                                <td><x-button2 type="button" value="614" onclick="number(614)">要介護5 4~5時間</x-button2></td>
+                                <td><x-button2 type="button" value="979" onclick="number(979)">要介護5 5~6時間</x-button2></td>
+                                <td><x-button2 type="button" value="1003" onclick="number(1003)">要介護5 6~7時間</x-button2></td>
+                                <td><x-button2 type="button" value="1142" onclick="number(1142)">要介護5 7~8時間</x-button2></td>
+                                <td><x-button2 type="button" value="1162" onclick="number(1162)">要介護5 8~9時間</x-button2></td>
                             </tr>
                     </table>
 
 
-                    <div class="mx-auto w-2/3 mt-4">
+                    <div class="mx-auto w-1/3 mt-4">
                         <x-label for="price" value="料金" />
-                        <x-input value="{{ $event->price }}" id="price" class="block mt-1 w-full" type="text" name="price" required />
+                        <x-input value="{{ $event->price }}" id="price" class="mt-1 w-4/5" type="text" name="price" required />円
                     </div>
                     
                     <script>
-                        // function number(){
-                            const nb = document.getElementById("button");
+                        function number($value){
                             var price = document.getElementById("price");
-                            
-                            // price = nb;
-                            console.log(price);
-
-                            price.value = 100;
-                        
+                            price.value = Math.floor($value * 10.72);
+                        }
                         </script>
 
                 <div class="mt-8 mx-auto w-1/6">
